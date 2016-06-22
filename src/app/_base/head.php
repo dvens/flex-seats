0ff3bb3e0e345679af1038344dce78b0dec54361
dvens <d.joelvens@gmail.com>
2016-06-23 00:36:52 +0200

    removed google fonts

--- src/app/_base/head.php
+++ src/app/_base/head.php
@@ -7,7 +7,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	
-	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700,900|Open+Sans:400italic,700italic,800italic,700,400,300,600' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="./css/styles.min.css">
 
 	<script>
